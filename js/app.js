0c60ff0927aaf4a4664504cf3b18022ea7c7b748
Rebeca Levy <beckalevy@Rebecas-MBP.attlocal.net>
2019-10-05 19:41:54 -0700

    got cards to flip

--- js/app.js
+++ js/app.js
@@ -10,6 +10,40 @@ console.log('page running')
 // }
 
 
+$('#start').on('click', ()=>{
+    console.log('button works')
+
+    game.setGameTimer()
+})
+$('.cards').on('click', ()=>{
+    console.log('card clicked')
+
+
+})
+
+
+
+const game = {
+    cards: [],
+    time: 0,
+    isCardFlipped: false,
+    setGameTimer(){
+        const $timer = $('.timer');
+        const interval = setInterval(()=>{
+            this.time++
+            $timer.text(`Time: ${this.time}`);
+        }, 1000)
+        if(this.time === 30){
+            clearInterval(interval);
+        }
+    },
+    flipCard(){
+
+
+    }
+}
+
+
 
 
 
