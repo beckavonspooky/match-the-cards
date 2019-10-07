8744719b25ce883331595a5770d9a33efe4db430
Rebeca Levy <beckalevy@Rebecas-MacBook-Pro.local>
2019-10-07 14:02:15 -0700

    created match Check

--- js/app.js
+++ js/app.js
@@ -1,29 +1,42 @@
-console.log('page running')
-
-// const game = {
-//     board =[
-//         [],
-//         [],
-//         []
-//     ]
-
-// }
-//cards: [resources/cards/barb.png, resources/cards/beetlejuice.png, resources/cards/lydia.png, resources/cards/missargentina.png, resources/cards/sandsnake.png, resources/cards/guide.png],
 
 $('#start').on('click', ()=>{
     console.log('button works')
 
     game.setGameTimer()
 })
-$('.cards').on('click', ()=>{
-    console.log('card clicked')
 
+$('.cards').on('click', (e)=>{
+    // console.log('card clicked')
+    console.log(e.target.name)
+    if(!game.firstCardFlipped) {
+        game.firstCardFlipped = e.target.name
+    } else {
+        game.secondCardFlipped = e.target.name
+        game.checkForMatch(game.firstCardFlipped, game.secondCardFlipped)
+        if(game.firstCardFlipped === game.secondCardFlipped){
+            console.log('Cards are a MATCH!')
+            game.firstCardFlipped='';
+            game.secondCardFlipped='';
+        } 
+        else {
+            game.firstCardFlipped != game.secondCardFlipped
+            console.log('NOT a match')
+            game.firstCardFlipped='';
+            game.secondCardFlipped='';
+        }
+    }
+    
 })
+
+
+
 
 const game = {
     cards: [],
     time: 0,
-    isCardFlipped: false,
+    firstCardFlipped: '',
+    secondCardFlipped: '',
+    flippedCard: false,
     setGameTimer(){
         const $timer = $('.timer');
         const interval = setInterval(()=>{
@@ -35,14 +48,29 @@ const game = {
         }, 1000) 
     },
     flipCard(){
+        
+        //if the first card is clicked, flip card, and keep the card from flipping
+        //if the second card is clicked, flip card, and keep the card from flipping back
 
+        
 
     },
-    checkForMatch(){
+    checkForMatch(card1, card2){
+        //compare two flipped cards
+        //if first card flipped image === second card flipped image, keep cards frontside
+        //esle reset to backside
+        // console.log(card1, card2, 'THESE ARE YO CARDS!!!!')
+
+        
+        
 
     },
     resetGame(){
+        //if the timer hits 30 and not all the cards are flipped, reset cards
         
+    },
+    shuffleCards(){
+
     }
 
 }
